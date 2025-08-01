5cdc18a64aa0f88d98bb9011e6db06ca3e5cd79f
abhisheksamriya <abhisheksamriya2004@gmail.com>
2025-08-01 13:28:09 +0530

    seo

---
+++ app/head.tsx
@@ -0,0 +1,16 @@
+// app/head.tsx
+export default function Head() {
+  return (
+    <>
+      <meta
+        name="google-site-verification"
+        content="Aumr9NyEUp9LJbUgQxWBsc7apiniigmUQUbWiFhvq2Y"
+      />
+      <title>Discover Resorts Karjat</title>
+      <meta
+        name="description"
+        content="Discover Resorts Karjat offers a serene escape with premium rooms, fine dining, spa facilities, and adventure activities. Perfect for family vacations, romantic getaways, and corporate retreats."
+      />
+    </>
+  );
+}
